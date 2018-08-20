2c52c29ab73feb68b11370047698cc95deca74ea
jalal <jimmy002020@gmail.com>
2018-08-20 17:06:59 +0200

    update

---
+++ src/components/cell/Cell.jsx
@@ -0,0 +1,254 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { keyGenerator } from '../../utils';
+
+import { TEXT, SELECT, LIST, CHECKBOX, RADIO, INPUT, BTN } from './constants';
+
+const propTypes = {
+  component: PropTypes.node,
+
+  type: PropTypes.string,
+  nameRef: PropTypes.string,
+  groupName: PropTypes.string,
+
+  value: PropTypes.string,
+  checked: PropTypes.bool,
+  id: PropTypes.string,
+
+  /** context props */
+  cellContext: PropTypes.shape({
+    registerCellInfo: PropTypes.func,
+    updateCellValue: PropTypes.func,
+    values: PropTypes.objectOf(PropTypes.string)
+  }),
+
+  onChange: PropTypes.func,
+  onBlur: PropTypes.func,
+
+  children: PropTypes.node
+};
+
+const defaultProps = {
+  component: null,
+
+  type: TEXT,
+  nameRef: null,
+  groupName: null,
+
+  value: '',
+  checked: false,
+  id: keyGenerator('autoID'),
+
+  /** context props */
+  cellContext: {
+    registerCellInfo() {},
+    updateCellValue() {},
+    values: {}
+  },
+
+  onChange: null,
+  onBlur: null,
+
+  children: null
+};
+
+/**
+ * Gets the cell type
+ * returns booleans type flage.
+ *
+ * @param {string} cell_type
+ * @return {Object} - isBtn, isSelect, isInput
+ */
+function recognizeCellType(type) {
+  let isBtn = false;
+  let isSelect = false;
+
+  if (type === SELECT || type === LIST) {
+    isSelect = true;
+  } else if (type === CHECKBOX || type === RADIO) {
+    isBtn = true;
+  }
+
+  return {
+    isSelect,
+    isBtn,
+    isInput: !isBtn && !isSelect
+  };
+}
+
+class Cell extends Component {
+  constructor(props) {
+    super(props);
+
+    const {
+      component: CellComponent,
+      nameRef,
+      value,
+      checked,
+      id,
+      cellContext: { registerCellInfo },
+      groupName,
+      type
+    } = props;
+
+    const { isBtn, isSelect, isInput } = recognizeCellType(type);
+
+    this.isBtn = isBtn;
+    this.isSelect = isSelect;
+    this.isInput = isInput;
+
+    /**
+     * assign render component
+     * input is default
+     */
+    if (!CellComponent) {
+      if (this.isSelect) {
+        this.CellComponent = SELECT;
+      } else {
+        this.CellComponent = INPUT;
+      }
+    } else {
+      // user choise
+      this.CellComponent = CellComponent;
+    }
+
+    // choose the init value that will be saved in context and local state
+    const localValue = isBtn ? checked : value;
+
+    // get unique reference name if nameRef is not provided
+    let artificialNameRe = nameRef;
+    if (!artificialNameRe) {
+      /*
+      * if there's id so be it
+      * otherwise, generate new one
+      * */
+      artificialNameRe = `${type}_${id}${groupName ? `_${groupName}` : ''}`;
+    }
+
+    // register cell info in context state
+    registerCellInfo({
+      nameRef: artificialNameRe,
+      iniValue: localValue,
+      groupName
+    });
+
+    this.nameRef = artificialNameRe;
+    this.valueRef = this.isBtn ? 'checked' : 'value';
+
+    this.state = { localValue };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    const { groupName } = this.props;
+    const {
+      cellContext: { values: { [this.nameRef]: contextValue } }
+    } = nextProps;
+
+    const { localValue } = this.state;
+
+    if (groupName && contextValue !== localValue) {
+      this.setState({
+        localValue: contextValue
+      });
+    }
+
+    return localValue !== nextState.localValue;
+  }
+
+  handleChange(e) {
+    const { target: { checked, value } } = e;
+    const {
+      groupName,
+      cellContext: { updateCellValue },
+      onChange
+    } = this.props;
+    const newValue = this.isBtn ? checked : value;
+
+    this.setState({
+      localValue: newValue
+    });
+
+    if (!this.isInput) {
+      updateCellValue({
+        nameRef: this.nameRef,
+        newValue,
+        cellType: this.isBtn ? BTN : SELECT,
+        groupName
+      });
+    }
+    if (onChange) {
+      onChange(e);
+    }
+  }
+
+  handleBlur(e) {
+    const { target: { value } } = e;
+    const { cellContext: { updateCellValue }, onBlur } = this.props;
+
+    updateCellValue({
+      nameRef: this.nameRef,
+      newValue: value,
+      cellType: INPUT,
+      groupName: null
+    });
+
+    if (onBlur) {
+      onBlur(e);
+    }
+  }
+
+  render() {
+    // console.log('cell update');
+
+    const {
+      component,
+
+      type,
+      nameRef,
+      groupName,
+
+      value,
+      checked,
+      id,
+
+      /** context props */
+      cellContext,
+
+      // NOTE: it must be another way to avoid passing unrequried props
+      // eslint-disable-next-line
+      formContext,
+
+      onChange,
+      onBlur,
+
+      children,
+      ...other
+    } = this.props;
+
+    const { localValue } = this.state;
+
+    const onBlurFunc = this.isInput ? this.handleBlur : onBlur;
+
+    const cellProps = {
+      type,
+      [this.valueRef]: localValue,
+      onChange: this.handleChange,
+      onBlur: onBlurFunc,
+      ...other
+    };
+    return this.isSelect ? (
+      <this.CellComponent {...cellProps}>{children}</this.CellComponent>
+    ) : (
+      <this.CellComponent {...cellProps} />
+    );
+  }
+}
+
+Cell.propTypes = propTypes;
+Cell.defaultProps = defaultProps;
+
+export default Cell;
