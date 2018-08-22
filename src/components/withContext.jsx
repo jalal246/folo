5221f4aa5f074f3778526731e82183513c66e594
jalal <jimmy002020@gmail.com>
2018-08-22 15:37:01 +0200

    add docs turns args to object accept specific props as an array

--- src/components/withContext.jsx
+++ src/components/withContext.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React from "react";
 
-function withContext(Component, Consumer) {
+/**
+ * HOC component
+ * Connect component to props
+ *
+ * @param {Component} targeted_component
+ * @param {Component} consumer_context
+ * @param {Array} contextProps contains props required from consumer
+ * @return {Component} - new component connected to context props
+ */
+function withContext({ Component, Consumer, contextProps = [] }) {
   return function ComponentWithContext(props) {
     return (
-      <Consumer>{context => <Component {...props} {...context} />}</Consumer>
+      <Consumer>
+        {context => {
+          /**
+           * if contextProps length is zero, pass all context props
+           * otherwise extract the required props
+           */
+          const cn =
+            contextProps.length > 0
+              ? contextProps.reduce((acc, val) => context[val], {})
+              : context;
+          return <Component {...props} {...cn} />;
+        }}
+      </Consumer>
     );
   };
 }
