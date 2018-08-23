05232ac27110c6d8958fbb7ec7c0b4c9f6cce1d2
jalal <jimmy002020@gmail.com>
2018-08-23 15:50:47 +0200

    only export consumer

--- src/components/grid/index.js
+++ src/components/grid/index.js
@@ -1,4 +1,4 @@
-export { default as GridItem } from './GridItem';
-export { default as Grid } from './Grid';
+export { default as GridItem } from "./GridItem";
+export { default as Grid } from "./Grid";
 
-export { GridConsumer, GridProvider } from './context';
+export { GridProvider } from "./context";
