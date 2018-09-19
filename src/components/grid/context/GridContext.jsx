a11724eed2c972f4ea039ba09f407926f35395d0
jalal <jimmy002020@gmail.com>
2018-09-19 19:30:42 +0200

    optimize

--- src/components/grid/context/GridContext.jsx
+++ src/components/grid/context/GridContext.jsx
@@ -10,64 +10,60 @@ export class GridProvider extends React.PureComponent {
 
     // store cells number accourding to its name
     this.cellPositions = {};
-    this.cellDefaultRow = {};
 
     this.biggestRowItem = 0;
   }
 
   /**
    * Auto set the row number
-   * If we dont have row then take the higher value
+   * If we dont have row then take the highest value
    * depending on biggestRowItem which updated with each grid item
    * Otherwise set the row do you have and update biggestRowItem
    *
    * This helps to assign position value according to highest value
    * If we start from 10, the next will be 11 and so on.
    *
-   * @param {String} key unique key for GridItem
-   * @param {Number} row row number
-   * @param {Number} toRow extends to row number
-   * @return {Number} position
+   * @param {object} GridItem -  GridItem that should be register and calculated
+   * @param {string} GridItem.key unique key generated in GridItem
+   * @param {string} GridItem.row
+   * @param {string} GridItem.toRow
    */
-  autoPositionCell = (key, row, toRow) => {
-    this.cellDefaultRow[key] = { row, toRow };
-
-    let isRowUpdated = false;
-    let isBiggestRowUpdated = false;
-
-    // if we have row
-    // set position & calculate the biggest
+  autoPositionCell = ({ key, row, toRow }) => {
+    /*
+    * if we have row,
+    * then set position & calculate the biggest
+    */
     if (row) {
+      /*
+      * if we have new value, then update
+      * update flag
+      * update biggestRowItem
+      */
       if (this.cellPositions[key] !== row) {
-        // if we have new Value
-        // update it and inform the flag
         this.cellPositions[key] = row;
-        isRowUpdated = true;
-      } else {
-        // do nothing, dont waste our time
-        return this.cellPositions[key];
       }
 
       if (row > this.biggestRowItem) {
         this.biggestRowItem = row;
-        isBiggestRowUpdated = true;
       }
-    }
-
-    if (toRow && toRow > this.biggestRowItem) {
-      this.biggestRowItem = toRow;
-      isBiggestRowUpdated = true;
-    }
-
-    // if we dont have row and toRow
-    // then auto increment
-    if (!isBiggestRowUpdated) {
-      this.biggestRowItem += 1;
-    }
-
-    // if not updated
-    // take the higher row value: biggestRowItem
-    if (!isRowUpdated) {
+      /*
+      * we dont have row so let's update biggestRowItem
+      *
+      * if toRow is the biggest so be it
+      */
+    } else {
+      if (toRow && toRow > this.biggestRowItem) {
+        this.biggestRowItem = toRow;
+      } else {
+        /*
+        * then auto increment
+        */
+        this.biggestRowItem += 1;
+      }
+      /*
+      * we are definitely didnt update row
+      * let's give it the biigest
+      */
       this.cellPositions[key] = this.biggestRowItem;
     }
 
