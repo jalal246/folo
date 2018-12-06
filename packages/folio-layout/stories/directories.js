2661686075e5c7021cb5d2472b7659de0a6d97d3
jalal <jimmy002020@gmail.com>
2018-12-06 21:34:35 +0200

    remove stories and add package name as main title

--- packages/folio-layout/stories/directories.js
+++
@@ -1,5 +0,0 @@
-export const GRID = "Grid types";
-// export const GRID_ITEM = "GridItem";
-export const GRID_EXPLICIT = "Explicit";
-export const GRID_IMPLICIT = "Implicit";
-export const MAIN_APP = "some examples";
--- packages/folio-layout/stories/gridExplicit.stories.js
+++ packages/folio-layout/stories/gridExplicit.stories.js
@@ -2,7 +2,9 @@ import React from "react";
 
 import { storiesOf } from "@storybook/react";
 import { text, number } from "@storybook/addon-knobs";
-import { GRID, GRID_EXPLICIT } from "./directories";
+
+import { MAIN_APP, GRID_EXPLICIT } from "./_directories";
+
 import { Grid } from "../src";
 
 const styleLabel = {
@@ -12,8 +14,17 @@ const styleLabel = {
   fontSize: "150%"
 };
 
-storiesOf(`${GRID}/${GRID_EXPLICIT}: Using Grid component`, module)
-  .add("columns", () => (
+const styleLabel2 = {
+  width: "172px",
+  height: "81px",
+  backgroundColor: " #444",
+  color: "#fff",
+  padding: "20px",
+  fontSize: "150%"
+};
+
+storiesOf(`${MAIN_APP}/${GRID_EXPLICIT}: Using Grid component`, module)
+  .add("4 columns", () => (
     <Grid
       style={{
         backgroundColor: "navajowhite"
@@ -30,12 +41,12 @@ storiesOf(`${GRID}/${GRID_EXPLICIT}: Using Grid component`, module)
       <div style={styleLabel}>8</div>
     </Grid>
   ))
-  .add("rows and columns", () => (
+  .add("3 rows and 3 columns", () => (
     <Grid
       style={{
         backgroundColor: "navajowhite"
       }}
-      col={number("col", 4)}
+      col={number("col", 3)}
       row={number("row", 3)}
     >
       <div style={styleLabel}>1</div>
@@ -46,25 +57,41 @@ storiesOf(`${GRID}/${GRID_EXPLICIT}: Using Grid component`, module)
       <div style={styleLabel}>6</div>
       <div style={styleLabel}>7</div>
       <div style={styleLabel}>8</div>
+      <div style={styleLabel}>9</div>
     </Grid>
   ))
-  .add("widths", () => (
+  .add("minimum widths", () => (
     <Grid
       style={{
-        gap: text("gap", "2em")
+        gap: text("gap", "0.3em")
       }}
-      col={number("col", 4)}
+      col={number("col", 3)}
       row={number("row", 2)}
       rowMinWidth={text("rowMinWidth", "140px")}
       colMinWidth={text("colMinWidth", "300px")}
     >
-      <div style={styleLabel}>1</div>
-      <div style={styleLabel}>2</div>
-      <div style={styleLabel}>3</div>
-      <div style={styleLabel}>4</div>
-      <div style={styleLabel}>5</div>
-      <div style={styleLabel}>6</div>
-      <div style={styleLabel}>7</div>
-      <div style={styleLabel}>8</div>
+      <div style={styleLabel2}>1</div>
+      <div style={styleLabel2}>2</div>
+      <div style={styleLabel2}>3</div>
+      <div style={styleLabel2}>4</div>
+      <div style={styleLabel2}>5</div>
+      <div style={styleLabel2}>6</div>
+    </Grid>
+  ))
+  .add("centered fixed widths", () => (
+    <Grid
+      style={{
+        gap: text("gap", "0")
+      }}
+      isCenter
+      col={number("col", 2)}
+      row={number("row", 2)}
+      rowWidth={text("rowWidth", "220px")}
+      colWidth={text("colWidth", "500px")}
+    >
+      <div style={styleLabel2}>2</div>
+      <div style={styleLabel2}>1</div>
+      <div style={styleLabel2}>3</div>
+      <div style={styleLabel2}>4</div>
     </Grid>
   ));
