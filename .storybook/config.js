9d1ad68d3b43cace8a3580e874c2350c833e0848
jalal <jimmy002020@gmail.com>
2018-12-11 18:01:54 +0200

    update config to include packages

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,49 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from "@storybook/react";
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/);
+import { withInfo } from "@storybook/addon-info";
+import { withNotes } from "@storybook/addon-notes";
+import { withKnobs } from "@storybook/addon-knobs";
+import { setOptions } from "@storybook/addon-options";
+
+const requests = [];
+
+// TODO: it must be a better way to do this!
+// see this: https://github.com/storybooks/storybook/issues/4890
+const folioValues = require.context(
+  "../packages/folio-values/stories",
+  true,
+  /.stories.js$/
+);
+
+const folioLayout = require.context(
+  "../packages/folio-layout/stories",
+  true,
+  /.stories.js$/
+);
+
+const folioForms = require.context(
+  "../packages/folio-forms/stories",
+  true,
+  /.stories.js$/
+);
+
+requests.push(folioValues);
+requests.push(folioLayout);
+requests.push(folioForms);
+
+setOptions({
+  name: "Folio",
+  url: "https://jalal246.github.io/folio/"
+});
+
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  addDecorator(withKnobs);
+  addDecorator(withNotes);
+  addDecorator(withInfo);
+
+  requests.forEach(req => {
+    req.keys().forEach(fname => req(fname));
+  });
 }
 
 configure(loadStories, module);
