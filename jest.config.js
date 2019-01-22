3220596b7b85e7ca81976a1efa4836c243446f70
jalal <jimmy002020@gmail.com>
2019-01-22 14:02:13 +0200

    fix jest coverage

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,5 @@ module.exports = {
 
   // An array of regexp pattern strings that are matched against all file paths before executing the test.
   // If the file path matches any of the patterns, coverage information will be skipped.
-  coveragePathIgnorePatterns: ["/node_modules/", "/dit/"]
+  coveragePathIgnorePatterns: ["/node_modules/", "/dist/"]
 };
