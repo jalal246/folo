5530784e8be9cbfa2d22f045847bf137ba215bb6
jalal246 <jimmy002020@gmail.com>
2020-12-18 22:21:15 +0200

    add new example for custom components

--- packages/folo-values/src/stories/FormsWithComponent.stories.js
+++ packages/folo-values/src/stories/FormsWithComponent.stories.js
@@ -1,83 +1,157 @@
+/* eslint-disable react/jsx-wrap-multilines */
 /* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from "react";
-
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
-import Button from "@material-ui/core/Button";
 
 import Field from "../components/Field";
 import Form from "../components/Form";
 
-const FormAcceptComponent = ({ onSubmit }) => (
-  <Form
-    storeID="FormAcceptComponent"
-    style={{
-      width: "60%",
-    }}
-    onSubmit={onSubmit}
-  >
-    <Field
-      component={TextField}
-      storeID="FormAcceptComponent"
-      label="username"
-      valueKey="username"
-      type="text"
-      margin="normal"
-    />
-
-    <List>
-      <ListItem key={0} dense button>
-        <Field
-          component={Checkbox}
-          storeID="FormAcceptComponent"
-          disableRipple
-          valueKey="NYC"
-          type="checkbox"
-          groupName="city"
-        />
-        <ListItemText primary="NYC" />
-        <ListItem key={1} dense button>
-          <Field
-            component={Checkbox}
-            storeID="FormAcceptComponent"
-            disableRipple
-            valueKey="LA"
-            type="checkbox"
-            groupName="city"
-          />
-          <ListItemText primary="LA" />
-        </ListItem>
-      </ListItem>
-    </List>
-
-    <Button
-      variant="contained"
-      type="submit"
-      style={{
-        width: "50%",
-        margin: "0 auto",
-      }}
-    >
-      submit
-    </Button>
-  </Form>
-);
-
 export default {
-  title: "Forms/Custom Component",
-  component: FormAcceptComponent,
-  argTypes: {
-    onSubmit: {
-      action: "onSubmit",
-    },
-    onClick: { action: "clicked" },
+  title: "Forms/Custom components",
+  component: Form,
+  onSubmit: {
+    action: "onSubmit",
   },
 };
 
-const Template = (args) => <FormAcceptComponent {...args} />;
-
-export const Basic = Template.bind({});
+export function AddressForm() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "60%",
+        }}
+      >
+        <Typography variant="h6" gutterBottom>
+          Shipping address
+        </Typography>
+        <Form>
+          <Grid container spacing={8}>
+            <Grid item xs={12} sm={6}>
+              <Field
+                component={TextField}
+                storeID="customComponent"
+                required
+                id="firstName"
+                name="firstName"
+                label="First name"
+                fullWidth
+                autoComplete="given-name"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Field
+                component={TextField}
+                storeID="customComponent"
+                required
+                id="lastName"
+                name="lastName"
+                label="Last name"
+                fullWidth
+                autoComplete="family-name"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Field
+                component={TextField}
+                storeID="customComponent"
+                required
+                id="address1"
+                name="address1"
+                label="Address line 1"
+                fullWidth
+                autoComplete="shipping address-line1"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Field
+                component={TextField}
+                storeID="customComponent"
+                id="address2"
+                name="address2"
+                label="Address line 2"
+                fullWidth
+                autoComplete="shipping address-line2"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Field
+                component={TextField}
+                storeID="customComponent"
+                required
+                id="city"
+                name="city"
+                label="City"
+                fullWidth
+                autoComplete="shipping address-level2"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Field
+                component={TextField}
+                storeID="customComponent"
+                id="state"
+                name="state"
+                label="State/Province/Region"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Field
+                component={TextField}
+                storeID="customComponent"
+                required
+                id="zip"
+                name="zip"
+                label="Zip / Postal code"
+                fullWidth
+                autoComplete="shipping postal-code"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Field
+                component={TextField}
+                storeID="customComponent"
+                required
+                id="country"
+                name="country"
+                label="Country"
+                fullWidth
+                autoComplete="shipping country"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={
+                  <Field
+                    component={Checkbox}
+                    type="checkbox"
+                    color="secondary"
+                    name="saveAddress"
+                    id="saveAddress"
+                    value="yes"
+                  />
+                }
+                label="Use this address for payment details"
+              />
+            </Grid>
+          </Grid>
+        </Form>
+      </div>
+    </div>
+  );
+}
