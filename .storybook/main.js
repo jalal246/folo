269ee97c3ca61f6af1fd8f8aa7422f7a1b0bdd61
jalal246 <jimmy002020@gmail.com>
2020-12-19 19:40:54 +0200

    Add global deps setting Jest & Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../packages/folo-values/src/stories/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
 };
