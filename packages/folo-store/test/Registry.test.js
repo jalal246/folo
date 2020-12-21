43696767b714977983a68cc215d4007616b45548
jalal246 <jimmy002020@gmail.com>
2020-12-21 20:26:26 +0200

    remove old sanps

--- packages/folo-store/test/Registry.test.js
+++ packages/folo-store/test/Registry.test.js
@@ -1,10 +1,10 @@
-import Registry from "../src/components/context/Registry";
+import Registry from "../src";
 
 const Register = new Registry();
 
-const { btnGroup, datatObj, registerCellInfo } = Register;
+const { btnGroup, dataObj, registerCellInfo } = Register;
 
-const MUTULAL_GROUP_MANE_1 = "gr1";
+const MUTUAL_GROUP_MANE_1 = "gr1";
 
 const NAME_REF_1 = "nameTest1";
 const INIT_VAL_1 = false;
@@ -12,7 +12,7 @@ const INIT_VAL_1 = false;
 const NAME_REF_2 = "nameTest2";
 const INIT_VAL_2 = true;
 
-const MUTULAL_GROUP_MANE_2 = "gr2";
+const MUTUAL_GROUP_MANE_2 = "gr2";
 
 const NAME_REF_3 = "nameTest3";
 const INIT_VAL_3 = true;
@@ -27,103 +27,103 @@ describe("Registry", () => {
     const obj1 = {
       nameRef: NAME_REF_1,
       initValue: INIT_VAL_1,
-      groupName: MUTULAL_GROUP_MANE_1
+      groupName: MUTUAL_GROUP_MANE_1,
     };
 
     registerCellInfo(obj1);
 
-    expect(datatObj).toMatchObject({ [NAME_REF_1]: INIT_VAL_1 });
-    expect(btnGroup[MUTULAL_GROUP_MANE_1].has(NAME_REF_1)).toBe(true);
+    expect(dataObj).toMatchObject({ [NAME_REF_1]: INIT_VAL_1 });
+    expect(btnGroup[MUTUAL_GROUP_MANE_1].has(NAME_REF_1)).toBe(true);
   });
   it("pushes second obj with same group name", () => {
     const obj2 = {
       nameRef: NAME_REF_2,
       initValue: INIT_VAL_2,
-      groupName: MUTULAL_GROUP_MANE_1
+      groupName: MUTUAL_GROUP_MANE_1,
     };
 
     registerCellInfo(obj2);
 
-    expect(datatObj).toMatchObject({
+    expect(dataObj).toMatchObject({
       [NAME_REF_1]: INIT_VAL_1,
-      [NAME_REF_2]: INIT_VAL_2
+      [NAME_REF_2]: INIT_VAL_2,
     });
-    expect(btnGroup[MUTULAL_GROUP_MANE_1].has(NAME_REF_1)).toBe(true);
-    expect(btnGroup[MUTULAL_GROUP_MANE_1].has(NAME_REF_2)).toBe(true);
+    expect(btnGroup[MUTUAL_GROUP_MANE_1].has(NAME_REF_1)).toBe(true);
+    expect(btnGroup[MUTUAL_GROUP_MANE_1].has(NAME_REF_2)).toBe(true);
   });
   it("tests btnGroup with 2 branches of group name", () => {
     const obj3 = {
       nameRef: NAME_REF_3,
       initValue: INIT_VAL_3,
-      groupName: MUTULAL_GROUP_MANE_2
+      groupName: MUTUAL_GROUP_MANE_2,
     };
 
     registerCellInfo(obj3);
 
-    expect(datatObj).toMatchObject({
+    expect(dataObj).toMatchObject({
       [NAME_REF_1]: INIT_VAL_1,
       [NAME_REF_2]: INIT_VAL_2,
-      [NAME_REF_3]: INIT_VAL_3
+      [NAME_REF_3]: INIT_VAL_3,
     });
-    expect(btnGroup[MUTULAL_GROUP_MANE_1].has(NAME_REF_1)).toBe(true);
-    expect(btnGroup[MUTULAL_GROUP_MANE_1].has(NAME_REF_2)).toBe(true);
-    expect(btnGroup[MUTULAL_GROUP_MANE_2].has(NAME_REF_3)).toBe(true);
+    expect(btnGroup[MUTUAL_GROUP_MANE_1].has(NAME_REF_1)).toBe(true);
+    expect(btnGroup[MUTUAL_GROUP_MANE_1].has(NAME_REF_2)).toBe(true);
+    expect(btnGroup[MUTUAL_GROUP_MANE_2].has(NAME_REF_3)).toBe(true);
   });
   it("tests registerCellInfo with non-grouped obj", () => {
     const obj4 = {
       nameRef: NAME_REF_4,
       initValue: INIT_VAL_4,
-      groupName: null
+      groupName: null,
     };
 
     registerCellInfo(obj4);
 
-    expect(datatObj).toMatchObject({
+    expect(dataObj).toMatchObject({
       [NAME_REF_1]: INIT_VAL_1,
       [NAME_REF_2]: INIT_VAL_2,
       [NAME_REF_3]: INIT_VAL_3,
-      [NAME_REF_4]: INIT_VAL_4
+      [NAME_REF_4]: INIT_VAL_4,
     });
-    expect(btnGroup[MUTULAL_GROUP_MANE_1].has(NAME_REF_1)).toBe(true);
-    expect(btnGroup[MUTULAL_GROUP_MANE_1].has(NAME_REF_2)).toBe(true);
-    expect(btnGroup[MUTULAL_GROUP_MANE_2].has(NAME_REF_3)).toBe(true);
+    expect(btnGroup[MUTUAL_GROUP_MANE_1].has(NAME_REF_1)).toBe(true);
+    expect(btnGroup[MUTUAL_GROUP_MANE_1].has(NAME_REF_2)).toBe(true);
+    expect(btnGroup[MUTUAL_GROUP_MANE_2].has(NAME_REF_3)).toBe(true);
   });
   it("tests registerCellInfo with non-grouped obj", () => {
     const obj4 = {
       nameRef: NAME_REF_4,
       initValue: INIT_VAL_4,
-      groupName: null
+      groupName: null,
     };
 
     registerCellInfo(obj4);
 
-    expect(datatObj).toMatchObject({
+    expect(dataObj).toMatchObject({
       [NAME_REF_1]: INIT_VAL_1,
       [NAME_REF_2]: INIT_VAL_2,
       [NAME_REF_3]: INIT_VAL_3,
-      [NAME_REF_4]: INIT_VAL_4
+      [NAME_REF_4]: INIT_VAL_4,
     });
-    expect(btnGroup[MUTULAL_GROUP_MANE_1].has(NAME_REF_1)).toBe(true);
-    expect(btnGroup[MUTULAL_GROUP_MANE_1].has(NAME_REF_2)).toBe(true);
-    expect(btnGroup[MUTULAL_GROUP_MANE_2].has(NAME_REF_3)).toBe(true);
+    expect(btnGroup[MUTUAL_GROUP_MANE_1].has(NAME_REF_1)).toBe(true);
+    expect(btnGroup[MUTUAL_GROUP_MANE_1].has(NAME_REF_2)).toBe(true);
+    expect(btnGroup[MUTUAL_GROUP_MANE_2].has(NAME_REF_3)).toBe(true);
   });
   it("tests registerCellInfo with same ref and group name obj and new value", () => {
     const obj5 = {
       nameRef: NAME_REF_3,
       initValue: INIT_VAL_5,
-      groupName: MUTULAL_GROUP_MANE_2
+      groupName: MUTUAL_GROUP_MANE_2,
     };
 
     registerCellInfo(obj5);
 
-    expect(datatObj).toMatchObject({
+    expect(dataObj).toMatchObject({
       [NAME_REF_1]: INIT_VAL_1,
       [NAME_REF_2]: INIT_VAL_2,
       [NAME_REF_3]: INIT_VAL_5,
-      [NAME_REF_4]: INIT_VAL_4
+      [NAME_REF_4]: INIT_VAL_4,
     });
-    expect(btnGroup[MUTULAL_GROUP_MANE_1].has(NAME_REF_1)).toBe(true);
-    expect(btnGroup[MUTULAL_GROUP_MANE_1].has(NAME_REF_2)).toBe(true);
-    expect(btnGroup[MUTULAL_GROUP_MANE_2].has(NAME_REF_3)).toBe(true);
+    expect(btnGroup[MUTUAL_GROUP_MANE_1].has(NAME_REF_1)).toBe(true);
+    expect(btnGroup[MUTUAL_GROUP_MANE_1].has(NAME_REF_2)).toBe(true);
+    expect(btnGroup[MUTUAL_GROUP_MANE_2].has(NAME_REF_3)).toBe(true);
   });
 });
--- packages/folo-values/test/Field.test.js
+++ packages/folo-values/test/Field.test.js
@@ -1,20 +1,15 @@
 import React from "react";
-import { fireEvent, cleanup } from "@testing-library/user-event";
-import { render, screen } from "@testing-library/react";
+import { render, fireEvent, screen, cleanup } from "@testing-library/react";
 
 import Field from "../src/components/Field";
 import Form from "../src/components/Form";
 
-const TEST_DEFAULT = "default";
+const TEST_TEXT_1 = "default";
 const TEST_BTN_1 = "btn1";
 const TEST_BTN_2 = "btn2";
 const TEST_FORM = "form";
 
-const ID_1 = "unique";
-const ID_2 = "unique2";
-
-const VAL_KEY = "myRefName";
-const GROUP_NAME = "gr";
+const GROUP_NAME = "group1";
 
 const NEW_VAL = "hello!";
 
@@ -22,19 +17,18 @@ const onSubmitMock = jest.fn((e, value) => value);
 
 const App = () => (
   <Form data-testid={TEST_FORM} onSubmit={onSubmitMock}>
-    <Field id={ID_2} data-testid={TEST_DEFAULT} />
-    <Field />
+    <Field id={TEST_TEXT_1} data-testid={TEST_TEXT_1} />
     <Field
       type="checkbox"
       groupName={GROUP_NAME}
-      id={ID_1}
+      id={TEST_BTN_1}
       data-testid={TEST_BTN_1}
     />
     <Field
       type="checkbox"
       checked
       groupName={GROUP_NAME}
-      valueKey={VAL_KEY}
+      valueKey={TEST_BTN_2}
       data-testid={TEST_BTN_2}
     />
   </Form>
@@ -45,11 +39,11 @@ let btn1;
 let btn2;
 let form;
 
-describe("Field#CellEngine & Form", () => {
-  beforeAll(() => {
+describe("Testing Fields & Form", () => {
+  beforeEach(() => {
     render(<App />);
 
-    txt = screen.getByTestId(TEST_DEFAULT);
+    txt = screen.getByTestId(TEST_TEXT_1);
 
     btn1 = screen.getByTestId(TEST_BTN_1);
     btn2 = screen.getByTestId(TEST_BTN_2);
@@ -57,87 +51,66 @@ describe("Field#CellEngine & Form", () => {
     form = screen.getByTestId(TEST_FORM);
   });
 
-  it("sets one btn to false doesn't toggle the other one which is false", () => {
-    // from
-    expect(btn1.checked).toBe(false);
-    expect(btn2.checked).toBe(true);
-
-    fireEvent.blur(btn2, { target: { checked: false } });
-
-    // to
-    expect(btn1.checked).toBe(false);
-    expect(btn2.checked).toBe(false);
-  });
-
-  it("sets one btn to true deesnt toggle the other one which is false", () => {
-    // from
-    expect(btn1.checked).toBe(false);
-    expect(btn2.checked).toBe(false);
-
-    fireEvent.blur(btn2, { target: { checked: true } });
-
-    // to
-    expect(btn1.checked).toBe(false);
-    expect(btn2.checked).toBe(true);
-  });
-
-  it("sets one btn to true, toggles the other to false", () => {
-    // from
-    expect(btn1.checked).toBe(false);
-    expect(btn2.checked).toBe(true);
-
-    fireEvent.blur(btn1, { target: { checked: true } });
-
-    // to
-    expect(btn1.checked).toBe(true);
-    expect(btn2.checked).toBe(false);
-  });
-
-  it("sets same value to btn", () => {
-    // from
-    expect(btn1.checked).toBe(true);
-    expect(btn2.checked).toBe(false);
-
-    fireEvent.blur(btn1, { target: { checked: true } });
-
-    // to
-    expect(btn1.checked).toBe(true);
-    expect(btn2.checked).toBe(false);
-  });
-
-  it("sets new value to text input", () => {
-    // from
-    expect(txt.value).toBe("");
-
-    fireEvent.change(txt, { target: { value: NEW_VAL } });
-
-    // to
-    expect(txt.value).toBe(NEW_VAL);
-  });
-
-  it("sets same value to text input", () => {
-    // from
-    expect(txt.value).toBe(NEW_VAL);
-
-    fireEvent.change(txt, { target: { value: NEW_VAL } });
-
-    // to
-    expect(txt.value).toBe(NEW_VAL);
-  });
-
-  it("return context value when sbmit", () => {
-    fireEvent.submit(form, {});
-
-    // to
-    expect(onSubmitMock).toHaveReturnedWith(
-      expect.objectContaining({
-        text_unique2: "",
-        // unknown_valueKey_created_at_1547750212936: "",
-        checkbox_unique_gr: true,
-        myRefName: false,
-      })
-    );
-
+  afterAll(() => {
     cleanup();
+  });
+
+  describe("Testing submit event on form for initial value", () => {
+    it("returns context value when submit", () => {
+      fireEvent.submit(form, {});
+
+      // to
+      expect(onSubmitMock).toHaveReturnedWith(
+        expect.objectContaining({
+          [TEST_TEXT_1]: "",
+          [TEST_BTN_1]: false,
+          [TEST_BTN_2]: true,
+        })
+      );
+    });
+  });
+
+  describe("Testing toggle group in buttons", () => {
+    it("Gets initial values", () => {
+      expect(btn1.checked).toBe(false);
+    });
+
+    it("Sets one btn to false doesn't toggle the other one which is false", () => {
+      fireEvent.blur(btn2, { target: { checked: false } });
+
+      expect(btn1.checked).toBe(false);
+      expect(btn2.checked).toBe(false);
+    });
+
+    it("Sets one btn to true doesn't toggle the other one which is false", () => {
+      fireEvent.blur(btn2, { target: { checked: true } });
+
+      expect(btn1.checked).toBe(false);
+      expect(btn2.checked).toBe(true);
+    });
+
+    it("Sets one btn to true, toggles the other to false", () => {
+      fireEvent.blur(btn1, { target: { checked: true } });
+      expect(btn1.checked).toBe(true);
+      expect(btn2.checked).toBe(false);
+    });
+
+    describe("Testing text input", () => {
+      it("Gets initial values", () => {
+        expect(txt.value).toBe("");
+      });
+
+      it("Sets new value to text input", () => {
+        fireEvent.change(txt, { target: { value: NEW_VAL } });
+
+        expect(txt.value).toBe(NEW_VAL);
+      });
+
+      it("sets the same value to text input", () => {
+        fireEvent.change(txt, { target: { value: NEW_VAL } });
+
+        expect(txt.value).toBe(NEW_VAL);
+      });
+    });
   });
 });
