8ed60e5dcd490698bb43707c524e7e236e3c1865
jalal246 <jimmy002020@gmail.com>
2020-12-22 02:08:00 +0200

    Add control addons for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,5 +3,9 @@ module.exports = {
     "../packages/folo-values/src/stories/*.stories.@(js|jsx|ts|tsx)",
     "../packages/folo-layout/src/stories/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-controls",
+  ],
 };
