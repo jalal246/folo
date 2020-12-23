be0042a5e827e414baca65c5bba9bef29b164921
jalal246 <jimmy002020@gmail.com>
2020-12-23 02:45:48 +0200

    fix export in layout

--- packages/folo-layout/src/index.js
+++ packages/folo-layout/src/index.js
@@ -1,4 +1,4 @@
-export { default as GridItem } from "./components/GridItem";
-export { default as Grid } from "./components/Grid";
+import GridItem from "./components/GridItem";
+import Grid from "./components/Grid";
 
-export { GridProvider as FoloLayout } from "./components/context";
+export { GridItem, Grid };
