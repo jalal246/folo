d2f751f34a8c9f2defa191e22ceb2fafe87a20d9
jalal246 <jimmy002020@gmail.com>
2020-12-28 20:38:24 +0200

    init project files

---
+++ packages/vue/folo-vue-values/src/constants.js
@@ -0,0 +1,11 @@
+export const VALUE = "value";
+export const CHECKED = "checked";
+
+export const TEXT = "text";
+
+export const INPUT = "input";
+export const SELECT = "select";
+export const LIST = "list";
+export const CHECKBOX = "checkbox";
+export const RADIO = "radio";
+export const BTN = "button";
